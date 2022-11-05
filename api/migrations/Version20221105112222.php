0ab9dff007d8d8cc400b218f8187cef99bda926a
MarwenTlili <marwentlili@gmail.com>
2022-11-05 12:27:46 +0100

    update schema

--- api/migrations/Version20221105112222.php
+++ api/migrations/Version20221105112222.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221030093930 extends AbstractMigration
+final class Version20221105112222 extends AbstractMigration
 {
     public function getDescription(): string
     {
