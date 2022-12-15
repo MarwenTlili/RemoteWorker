612a3248493b0097f6d8070775f89ae7629d4427
MarwenTlili <marwentlili@gmail.com>
2022-12-15 12:59:06 +0100

    optimize user show

--- pwa/components/admin/users/UserShow.tsx
+++ pwa/components/admin/users/UserShow.tsx
@@ -1,15 +1,41 @@
 import { useParams } from 'react-router-dom';
-import { useGetOne, useRedirect, Title, Loading } from 'react-admin';
 import {
-  Card,
-  CardContent,
-  Stack,
+  useGetOne,
+  useRedirect,
+  Loading,
+  EditButton,
+  Show,
+  SimpleShowLayout,
+  TextField,
+  TopToolbar,
+  DeleteWithConfirmButton,
+  useRecordContext
+} from 'react-admin';
+import {
   Typography,
-  Container,
-  Box
+  useMediaQuery
 } from '@mui/material';
 
+const CustomToolbar = () => {
+  const record = useRecordContext();
+  // the record can be empty while loading
+  if (!record) return null;
+  return (
+    <TopToolbar>
+      <EditButton label='Edit' />
+      <DeleteWithConfirmButton
+        label='Delete'
+        confirmTitle={"Delete User '" + record.username + "'"}
+        confirmContent="After delete the user, it cannot be recovered. Are you sure?"
+        translateOptions={{ name: record.username }}
+      />
+    </TopToolbar>
+  )
+}
+
 export const UserShow = () => {
+  const isSmall = useMediaQuery((theme: any) => theme.breakpoints.down("sm"));
+
   const { id } = useParams();
   const redirect = useRedirect();
   const { data, isLoading } = useGetOne(
@@ -21,48 +47,32 @@ export const UserShow = () => {
   if (isLoading) { return <Loading />; }
 
   return(
-    <div>
-      <Title title="User Show" />
+    <Show actions={<CustomToolbar />} title={data.username}>
+      <SimpleShowLayout>
+        <TextField source="username" />
+        <TextField source="email" />
 
-      <Box component="span" sx={{ p: 4, border: 'none' }}>
-        <Container maxWidth="sm">
-          <Card>
-            <CardContent>
-              <Stack spacing={2} >
-                <div>
-                  <Typography variant="caption" display="block">Username</Typography>
-                  <Typography variant="h5">{data.username}</Typography>
-                </div>
-                <div>
-                  <Typography variant="caption" display="block">E-Mail</Typography>
-                  <Typography variant="h5">{data.email}</Typography>
-                </div>
-                <div>
-                  <Typography variant='caption' display='block'>Roles</Typography>
-                  {data.roles.map((role: any) => {
-                    switch (role) {
-                      case "ROLE_ADMIN":
-                        return <Typography key={role} variant='h5' color="red" >admin </Typography>;
-                      case "ROLE_CLIENT":
-                        return <Typography key={role} variant='h5' color="blue">client </Typography>;
-                      case "ROLE_ENGINEER":
-                        return <Typography key={role} variant='h5' color="orange">engineer </Typography>;
-                      default:
-                        return <Typography key={0} variant='h5' color="error">UNDEFINED </Typography>;
-                    }
-                  })}
-                </div>
-                <div>
-                  <Typography variant='caption' display='block'>Encrypted Password</Typography>
-                  <Typography variant='body2'>{ data.password }</Typography>
-                </div>
-              </Stack>
-            </CardContent>
-          </Card>
-        </Container>
-      </Box>
+        <div>
+          <Typography variant='caption' display='block'>Roles</Typography>
+          {data.roles.map((role: any) => {
+            switch (role) {
+              case "ROLE_ADMIN":
+                return <Typography key={role} variant='body2' color="red" >admin </Typography>;
+              case "ROLE_CLIENT":
+                return <Typography key={role} variant='body2' color="blue">client </Typography>;
+              case "ROLE_ENGINEER":
+                return <Typography key={role} variant='body2' color="orange">engineer </Typography>;
+              default:
+                return <Typography key={0} variant='body2' color="error">UNDEFINED </Typography>;
+            }
+          })}
+        </div>
 
-    </div>
+        <TextField source="password" />
+      </SimpleShowLayout>
+    </Show>
+
   );
 }
+
 
