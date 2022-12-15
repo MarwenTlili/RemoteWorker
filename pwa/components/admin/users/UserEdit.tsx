9ba764369055175089db8d509cb698df68687eea
MarwenTlili <marwentlili@gmail.com>
2022-12-15 13:21:21 +0100

    optimize UserEdit form tollbar

--- pwa/components/admin/users/UserEdit.tsx
+++ pwa/components/admin/users/UserEdit.tsx
@@ -4,13 +4,34 @@ import {
   SimpleForm,
   TextInput,
   required,
-  AutocompleteArrayInput
+  AutocompleteArrayInput,
+  useRecordContext,
+  DeleteWithConfirmButton,
+  SaveButton,
+  Toolbar
 } from 'react-admin';
+
+const CustomToolbar = () => {
+  const record = useRecordContext();
+  // the record can be empty while loading
+  if (!record) return null;
+  return (
+    <Toolbar>
+      <SaveButton label="save" />
+      <DeleteWithConfirmButton
+        label="Delete"
+            confirmTitle={"Delete User '" + record.username + "'"}
+          confirmContent="After delete the user, it cannot be recovered. Are you sure?"
+          translateOptions={{ name: record.username }}
+      />
+    </Toolbar>
+  )
+}
 
 export const UserEdit = () => {
   return (
-    <Edit>
-      <SimpleForm>
+    <Edit >
+      <SimpleForm toolbar={<CustomToolbar />} >
         {/* <TextInput disabled label="Id" source="id" /> */}
         <TextInput source="username" validate={required()} />
         <TextInput source="email" validate={required()} />
